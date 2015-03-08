d9de9d3f6018aad0173f5bdf6a3de501eb81b91a
JackieChiles <dtgaynor@gmail.com>
2015-03-08 16:40:24 -0400

    Added schema for #87.

---
+++ app/server/schema.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var dbName = require('./config').dbName;
+var mongoose = require('mongoose');
+    
+mongoose.connect(dbName, function (err) {
+    if (err) {
+        console.log('Error connecting to database: ' + err);
+    }
+});
+    
+module.exports = {
+    Quilt: mongoose.model('Quilt', new mongoose.Schema({
+        name: String,
+        creationDate: { type: Date, default: Date.now },
+        width: Number,
+        height: Number
+    }))
+};
